bba4c8addb7c4c59ee56d5b28000ae184463caba
Martin Chorley <chorleymj@cardiff.ac.uk>
2020-10-17 21:56:27 +0100

    messing with tailwing

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const autoprefixer = require('autoprefixer')
+const tailwindcss = require('tailwindcss')
+const cssnano = require('cssnano')
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {},
+  },
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+        display: ["Scope One", 'serif'],
+        body: ["Roboto", 'sans-serif'],
+    },
+    container : {
+        center: true,
+    },
+    extend: {
+        colors: {
+           brand: '#d3374a', 
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
